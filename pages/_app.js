836c2964955de73c19e2650b84088a3865bb7f65
Abdelkrim Temam <gm_temam@esi.dz>
2020-09-09 17:34:02 +0100

    added standardjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/main.css';
+import '../styles/main.css'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp ({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
