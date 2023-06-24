a10a76cf01ffc7177a0ab70eff724e94dfee08b9
Krimo TEMAM <krimolinch@gmail.com>
2023-06-24 13:14:54 +0100

    nextjs 13 clean slate

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
-# nextjs-tailwindcss template
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-next.js template including tailwindcss, **check out** the [jsx-tailwind](https://github.com/andybroger/nextjs-tailwindcss/tree/jsx-tailwind) branch if you need support for @apply rules in styled-jsx.
+## Getting Started
 
-**Features**
+First, run the development server:
 
-- Includes Tailwindcss
-- Includes postcss-preset-env
-- now uses tailwindcss 1.4.x purge method on production builds
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
 
-## usage
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-1. Clone this repo
-2. npm install
-3. npm run dev
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-## notes
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
-**styles/main.css**
-The styles/main.css files includes tailwindcss imports and also supports global styles. It is processed by postcss and with postcss-preset-env supports nesting and other cool stuff.
+## Learn More
 
-**postcss.config.js**
-The configuration file for postcss.
+To learn more about Next.js, take a look at the following resources:
 
-**tailwind.config.js**
-You should know that file, its the default config generated with `npx tailwindcss init`. It's where your tailwindcss config goes.
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-**pages/\_app.js**
-Here we integrate `styles/main.css` into the app.
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,18 @@
-const { spacing, fontFamily } = require('tailwindcss/defaultTheme')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
-  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
-      colors: {
-        'blue-opaque': 'rgb(13 42 148 / 18%)'
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans]
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.gray.700'),
-            a: {
-              color: theme('colors.blue.500'),
-              '&:hover': {
-                color: theme('colors.blue.700')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            'h2,h3,h4': {
-              'scroll-margin-top': spacing[32]
-            },
-            code: { color: theme('colors.pink.500') },
-            'blockquote p:first-of-type::before': false,
-            'blockquote p:last-of-type::after': false
-          }
-        },
-        dark: {
-          css: {
-            color: theme('colors.gray.300'),
-            a: {
-              color: theme('colors.blue.400'),
-              '&:hover': {
-                color: theme('colors.blue.600')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            blockquote: {
-              borderLeftColor: theme('colors.gray.700'),
-              color: theme('colors.gray.300')
-            },
-            'h2,h3,h4': {
-              color: theme('colors.gray.100'),
-              'scroll-margin-top': spacing[32]
-            },
-            hr: { borderColor: theme('colors.gray.700') },
-            ol: {
-              li: {
-                '&:before': { color: theme('colors.gray.500') }
-              }
-            },
-            ul: {
-              li: {
-                '&:before': { backgroundColor: theme('colors.gray.500') }
-              }
-            },
-            strong: { color: theme('colors.gray.300') },
-            thead: {
-              color: theme('colors.gray.100')
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme('colors.gray.700')
-              }
-            }
-          }
-        }
-      })
-    }
+    },
   },
-  variants: {
-    typography: ['dark']
-  },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [],
 }
